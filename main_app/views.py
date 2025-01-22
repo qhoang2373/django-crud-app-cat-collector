938e3159aaf2981470866b3d7d59b07d1a557fe5
Quan <qhoang2373@gmail.com>
2025-01-21 20:13:33 -0800

    set up about base.html partials

--- main_app/views.py
+++ main_app/views.py
@@ -8,6 +8,8 @@ def home(request):
     # Send a simple HTML response
     return HttpResponse('<h1>Hello ᓚᘏᗢ</h1>')
 
-def about(request):
-      return HttpResponse("<h1>About the CatCollector</h1>")
+# def about(request):
+#       return HttpResponse("<h1>About the CatCollector</h1>")
 
+def about(request):
+    return render(request, 'about.html')
