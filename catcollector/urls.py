abd998f1de6aab23ec39cad958818bfecbb63460
Quan <qhoang2373@gmail.com>
2025-01-21 21:11:05 -0800

    complete about page

--- catcollector/urls.py
+++ catcollector/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main_app.urls')), # Mounts main_app's routes at the root URL
+    path('', include('main_app.urls')),
 ]
